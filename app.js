732c4e9d859b31ae33e4934c23521be5b87e8144
Fandy Saputra <85758259+fandyyuliawans@users.noreply.github.com>
2026-06-13 00:11:08 +0700

    Update app.js

--- app.js
+++ app.js
@@ -65,7 +65,7 @@ document.getElementById('btn-tambah-storage').addEventListener('click', async ()
 
         // PENTING: Ganti dengan Client ID Google dan ID Supabase Anda!
         const GOOGLE_CLIENT_ID = "MASUKKAN_CLIENT_ID_GOOGLE_ANDA_DI_SINI"; 
-        const REDIRECT_URI = "https://[KODE-PROYEK-ANDA].supabase.co/functions/v1/google-auth-callback";
+        const REDIRECT_URI = "https://dmagkklzsjfmuposfulb.supabase.co/functions/v1/google-auth-callback";
         
         const googleAuthUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${GOOGLE_CLIENT_ID}&redirect_uri=${encodeURIComponent(REDIRECT_URI)}&response_type=code&scope=https://www.googleapis.com/auth/drive.file%20email&access_type=offline&state=${session.user.id}&prompt=consent`;
         window.location.href = googleAuthUrl; 
